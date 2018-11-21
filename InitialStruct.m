2dc5aa932490a817896cbda0d974ee352b645b0c
TomWilk257 <45198863+TomWilk257@users.noreply.github.com>
2018-11-21 11:21:29 +0000

    Add files via upload

---
+++ InitialStruct.m
@@ -0,0 +1,18 @@
+tic
+%Run freq. analysis of case, built into Freq_Analysis.py
+dos('abaqus cae noGUI=Freq_Analysis.py');
+
+%Run matlab file to convert mass and stiff matrices to get modal forces as
+%.csv file to plug back into next abaqus file
+for i = 1:10
+mtxread_mk1(i);
+
+%Get modal displacements 
+dos('abaqus cae noGUI=Freq_Analysis_Copy.py');
+
+%Extract results from previous step
+dos('abaqus cae noGUI=readdispoutput.py')
+movefile('results1.csv',['Modeshape',num2str(i),'.csv'])
+i
+end
+toc
