c746a9efb570949bd80bffd4a2438952cd9a737a
vm15717 <vm15717@my.bristol.ac.uk>
2018-11-27 18:38:22 +0000

    Add files via upload

--- InitialStruct.m
+++ InitialStruct.m
@@ -6,7 +6,7 @@ dos('abaqus cae noGUI=Freq_Analysis.py');
 %.csv file to plug back into next abaqus file
 p=0;
 af=linspace(-1,1,10);
-for k= 1e-1:1e-1:1e+0
+for k= 5e-2:2e-1:2e+0
     for i = 1:length(af)
         p=p+1;
         forcecalc(Phi,Lam,Midx,diagKs,af(i)*k)
@@ -14,7 +14,7 @@ for k= 1e-1:1e-1:1e+0
         dos('abaqus cae noGUI=Freq_Analysis_Copy.py');
         %Extract results from previous step
         dos('abaqus cae noGUI=readdispoutput.py');
-        movefile('results1.csv',['force',num2str(p),'.csv']);
+        movefile('results1.csv',['force',num2str(p),'.csv'],'f');
         p
     end
 end
